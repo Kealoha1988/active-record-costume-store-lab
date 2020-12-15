2143a101904c5b78421e8127a9bd4adb3da8b82d
Kealoha1988 <kealohaabe1988@gmail.com>
2020-12-15 16:28:41 -0500

    commit tree cd80c81c9324c0628f46d834ee2abb7ba2a7608f

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -7,5 +7,19 @@
 # Define a change method in which to do the migration
 # In this change method, create columns with the correct names and 
 # value types according to the spec
+class CreateCostumes < ActiveRecord::Migration[4.2]
+
+    def change
+        create_table :costumes do |t|
+            t.string :name
+            t.integer :price
+            t.string :size
+            t.string :image_url 
+            t.timestamp :created_at
+            t.timestamp :updated_at
+        end
+    end
+
+end
 
 # !!! Before you run rake db:migrate, remember to fill out the other migration files -- otherwise you'll get an error resulting from the blank migration files.
